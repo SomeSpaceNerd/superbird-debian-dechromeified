1031972e8d16545ac7d20070f82351e0fd353b81
wil-sys <52981362+SomeSpaceNerd@users.noreply.github.com>
2024-07-28 02:15:08 -0400

    removed app
    it did not work :/

--- files/data/scripts/run_on_start_settings.sh
+++ files/data/scripts/run_on_start_settings.sh
@@ -1,4 +1,4 @@
 # settings for /scripts/run_on_start.sh
-PROGRAM_BINARY = "./scripts/test_app/mpy/micropython /scripts/test_app/src/ui.py"
+PROGRAM_BINARY = ""
 EXTRA_PROGRAM_ARGS=""
 EXTRA_XORG_ARGS="-nocursor"
